f4d8ec48af35b06305760fc5584d4f41aeda383f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2019-02-25 12:24:41 +0100

    odoo_website_custom_grid

---
+++ odoo_website_custom_grid/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Part of Probuse Consulting Service Pvt Ltd. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'custom grid for NOAH',
+    'version': '1.0',
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'Other proprietary',
+    'author' : 'SerinCloud S.L',
+    'website' : 'ingenieriacloud.com',
+    'support': 'pedro.guirao@ingenieriacloud.com',
+    'images': ['static/description/img1.jpeg'],
+    'live_test_url': 'https://www.youtube.com',
+    'depends': [
+        'website',     
+        'portal',
+        
+    ],
+    'data': [
+        
+     ],
+    'category': 'vistas de productos',
+    'summary':  """--IC presentación de productos--.""",
+    'description': """
+
+
+     """,
+    'installable': True,
+    'application': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ odoo_website_custom_grid/controllers/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from openerp.addons.website_sale.controllers import main
+
+main.PPR=2
+main.PPG=4
+
+# vim:expandtab:tabstop=4:softtabstop=4:shiftwidth=4: 
+
